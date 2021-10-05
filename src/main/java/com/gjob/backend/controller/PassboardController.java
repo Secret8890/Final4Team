26e3bcb89006473887fd63a3789c08791a69a448
eeunhui <sincerityissc4ry@gmail.com>
2021-10-05 14:44:36 +0900

    합격 자소서 및 카드 디자인 변경

--- src/main/java/com/gjob/backend/controller/PassboardController.java
+++ src/main/java/com/gjob/backend/controller/PassboardController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import lombok.AllArgsConstructor;
 
@@ -33,29 +34,30 @@ public class PassboardController {
 
         Map<String, Object> returnMap = new HashMap<String, Object>();
         returnMap.put("board", passboardService.selectAjaxByHitS(pagerMap));
-        returnMap.put("pager", pager);
+        returnMap.put("pager", pageNum);
         ModelAndView mv = new ModelAndView("success/success_index");
         mv.addObject("map", returnMap);
         return mv;
     }
 
     // @GetMapping("/listGet")
-    // public @ResponseBody Map<String, Object> passboardList(@RequestParam(defaultValue = "1") int pageNum) {
-    //     int totalBoard = passboardService.selectCountS();
-    //     int pageSize = 16;
-    //     int blockSize = 5;
+    // public @ResponseBody Map<String, Object>
+    // passboardList(@RequestParam(defaultValue = "1") int pageNum) {
+    // int totalBoard = passboardService.selectCountS();
+    // int pageSize = 16;
+    // int blockSize = 5;
 
-    //     Pager pager = new Pager(pageNum, totalBoard, pageSize, blockSize);
+    // Pager pager = new Pager(pageNum, totalBoard, pageSize, blockSize);
 
-    //     Map<String, Object> pagerMap = new HashMap<String, Object>();
-    //     pagerMap.put("startRow", pager.getStartRow());
-    //     pagerMap.put("endRow", pager.getEndRow());
+    // Map<String, Object> pagerMap = new HashMap<String, Object>();
+    // pagerMap.put("startRow", pager.getStartRow());
+    // pagerMap.put("endRow", pager.getEndRow());
 
-    //     Map<String, Object> returnMap = new HashMap<String, Object>();
-    //     returnMap.put("board", passboardService.selectAjaxByHitS(pagerMap));
-    //     returnMap.put("pager", pager);
+    // Map<String, Object> returnMap = new HashMap<String, Object>();
+    // returnMap.put("board", passboardService.selectAjaxByHitS(pagerMap));
+    // returnMap.put("pager", pager);
 
-    //     return returnMap;
+    // return returnMap;
     // }
 
     @GetMapping("/content")
