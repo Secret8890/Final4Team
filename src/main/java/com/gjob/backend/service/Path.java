a9517df2d95ec93a7b530cf9bcec61176a7130ab
ziageem <ziageem@gmail.com>
2021-10-10 02:03:22 +0900

    채용 공고 검색

--- src/main/java/com/gjob/backend/service/Path.java
+++ src/main/java/com/gjob/backend/service/Path.java
@@ -1,5 +1,5 @@
 package com.gjob.backend.service;
 
 public class Path {
-	public static final String FILE_STORE = "C:\\Users\\ASUS\\Desktop\\workSpace\\Project\\demo\\src\\main\\resources\\static\\video\\";
+	public static final String FILE_STORE = "C:\\Users\\1\\Desktop\\gjob-project\\src\\main\\resources\\static\\audio\\";
 }
