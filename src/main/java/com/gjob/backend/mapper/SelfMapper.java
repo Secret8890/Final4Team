263eba1f2f6833f8abc113821873337f023780ff
Secret8890 <gml8890@naver.com>
2021-10-10 03:14:39 +0900

    자소서 동적인 추가 및 삭제 insert문 처리 JsonObject Parsing

--- src/main/java/com/gjob/backend/mapper/SelfMapper.java
+++ src/main/java/com/gjob/backend/mapper/SelfMapper.java
@@ -2,7 +2,7 @@ package com.gjob.backend.mapper;
 
 import java.util.List;
 
-import com.gjob.backend.model.SelfDTO;
+import com.gjob.backend.model.*;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
@@ -14,7 +14,7 @@ public interface SelfMapper {
     List<SelfDTO> selectCompany();
     List<SelfDTO> selectQA(SelfDTO selfdto);
     void insertSelf(SelfDTO selfdto);
-    void insertQA(SelfDTO selfdto);
+    void insertQA(QuesDTO quesdto);
     void update(SelfDTO selfdto);
     void deleteQA(int qa_seq);
     void deleteSelf(int self_seq);
--- src/main/java/com/gjob/backend/service/SelfServiceImpl.java
+++ src/main/java/com/gjob/backend/service/SelfServiceImpl.java
@@ -3,8 +3,12 @@ package com.gjob.backend.service;
 import java.util.List;
 
 import com.gjob.backend.mapper.SelfMapper;
+import com.gjob.backend.model.QuesDTO;
 import com.gjob.backend.model.SelfDTO;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,15 +18,29 @@ public class SelfServiceImpl implements SelfService {
     SelfMapper mapper;
 
     @Override
-    public void insertSelfS(SelfDTO selfdto){
-        mapper.insertSelf(selfdto);
-        mapper.insertQA(selfdto);
+    public void insertSelfS(SelfDTO selfdto, JSONArray jsonQues) {
+        try{
+            int count = mapper.maxSelf();
+            mapper.insertSelf(selfdto);
+            JSONParser parser = new JSONParser();
+            for(int i=0;i<jsonQues.size();i++){
+                QuesDTO ques = new QuesDTO();
+                JSONObject obj = (JSONObject)parser.parse(jsonQues.get(i).toString());
+                ques.setQa_q(obj.get("qa_q").toString());
+                ques.setQa_a(obj.get("qa_a").toString());
+                ques.setSelf_seq(count);
+                mapper.insertQA(ques);
+            }
+        } catch(Exception e) {
+            System.out.println("service Parse Exception" + e);
+        }
+        //mapper.insertQA(selfdto);
         
     }
 
     @Override
-    public void insertQAS(SelfDTO selfdto){
-        mapper.insertQA(selfdto);
+    public void insertQAS(QuesDTO quesdto){
+        mapper.insertQA(quesdto);
     }
 
     @Override
