329f5c3e77a1da5eb98202867ef6ed28f593c39a
ziageem <ziageem@gmail.com>
2021-10-19 15:04:34 +0900

    크롤링 logo 오류 수정

--- src/main/java/com/gjob/backend/service/CompanyService.java
+++ src/main/java/com/gjob/backend/service/CompanyService.java
@@ -20,6 +20,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -320,10 +321,15 @@ public class CompanyService {
             CrawlingDTO crawlingdto = new CrawlingDTO();
             driver.get(url);
             // Thread.sleep(2000);
-            element9 = driver.findElement(By.cssSelector("div.logo"));
-            html_7 = element9.getAttribute("innerHTML"); // 7. 로고
-            System.out.println("element9 : " + element9);
-            System.out.println("html_7 : " + html_7);
+            try {
+                element9 = driver.findElement(By.cssSelector("div.logo"));
+                html_7 = element9.getAttribute("innerHTML"); // 7. 로고
+                System.out.println("element9 : " + element9);
+                System.out.println("html_7 : " + html_7);
+            } catch (NoSuchElementException ne) {
+                System.out.println("Logo NoSuchElementException");
+            }
+
             element = driver.findElement(By.xpath("//*[@id=\"iframe_content_0\"]"));
             driver.switchTo().frame(element); // iframe 안의 내용 출력
             element2 = driver.findElement(By.xpath("/html/body/div"));
